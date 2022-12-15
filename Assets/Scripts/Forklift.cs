bbf7e0c835bb3dc89421a08c78e2585b959fe572
lyndonpett <lyndonpett98@gmail.com>
2022-12-15 13:52:07 -0600

    Adding a moving forkboi! - Still need to add wheel rotating

--- Assets/Scripts/Forklift.cs
+++ Assets/Scripts/Forklift.cs
@@ -9,20 +9,36 @@ public class Forklift : MonoBehaviour
     public float turnspeed = 3f;
     public float forkSpeed = 2f;
     public float forkStartPos;
-    public Rigidbody LFwheel, RFwheel, LBwheel, RBwheel;
+    public GameObject WheelColliders, Wheels;
+    public WheelCollider LFwheel, RFwheel, LBwheel, RBwheel;
+    public GameObject LFwheelMesh, RFwheelMesh, LBwheelMesh, RBwheelMesh;
     private void Start()
     {
         fork = transform.GetChild(0).gameObject;
-        LBwheel = transform.GetChild(2).gameObject.GetComponent<Rigidbody>();
-        LFwheel = transform.GetChild(3).gameObject.GetComponent<Rigidbody>();
-        RBwheel = transform.GetChild(4).gameObject.GetComponent<Rigidbody>();
-        RFwheel = transform.GetChild(5).gameObject.GetComponent<Rigidbody>();
+        WheelColliders = transform.GetChild(3).gameObject;
+        Wheels = transform.GetChild(2).gameObject;
+        LBwheel = WheelColliders.transform.GetChild(0).gameObject.GetComponent<WheelCollider>();
+        LFwheel = WheelColliders.transform.GetChild(1).gameObject.GetComponent<WheelCollider>();
+        RBwheel = WheelColliders.transform.GetChild(2).gameObject.GetComponent<WheelCollider>();
+        RFwheel = WheelColliders.transform.GetChild(3).gameObject.GetComponent<WheelCollider>();
+        LBwheelMesh = Wheels.transform.GetChild(0).gameObject;
+        LFwheelMesh = Wheels.transform.GetChild(1).gameObject;
+        RBwheelMesh = Wheels.transform.GetChild(2).gameObject;
+        RFwheelMesh = Wheels.transform.GetChild(3).gameObject;
         forkStartPos = fork.transform.position.y;
     }
 
     // move up and down
     private void Update()
     {
+        LFwheelMesh.transform.position = LFwheel.transform.position;
+        LFwheelMesh.transform.rotation = LFwheel.transform.rotation;
+
+        RFwheelMesh.transform.position = RFwheel.transform.position;
+        RFwheelMesh.transform.rotation = RFwheel.transform.rotation;
+
+        LBwheelMesh.transform.rotation = LBwheel.transform.rotation;
+        RBwheelMesh.transform.rotation = RBwheel.transform.rotation;
 
         if (Input.GetKey(KeyCode.E) && fork.transform.position.y < forkStartPos + 8)
         {
@@ -35,10 +51,39 @@ public class Forklift : MonoBehaviour
 
         if (Input.GetKey(KeyCode.W))
         {
-            LBwheel.AddTorque(Vector3.forward);
-            LFwheel.AddTorque(Vector3.forward);
-            RBwheel.AddTorque(Vector3.forward);
-            RFwheel.AddTorque(Vector3.forward);
+            LBwheel.motorTorque = 500f;
+            LFwheel.motorTorque = 500f;
+            RBwheel.motorTorque = 500f;
+            RFwheel.motorTorque = 500f;       
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            LBwheel.motorTorque = -500f;
+            LFwheel.motorTorque = -500f;
+            RBwheel.motorTorque = -500f;
+            RFwheel.motorTorque = -500f;
+        }
+        else
+        {
+            LBwheel.motorTorque = 0f;
+            LFwheel.motorTorque = 0f;
+            RBwheel.motorTorque = 0f;
+            RFwheel.motorTorque = 0f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            LFwheel.steerAngle -= 10f * Time.deltaTime;
+            RFwheel.steerAngle -= 10f * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            LFwheel.steerAngle += 10f * Time.deltaTime;
+            RFwheel.steerAngle += 10f * Time.deltaTime;
+        }
+        else
+        {
+            LFwheel.steerAngle = 0f;
+            RFwheel.steerAngle = 0f;
         }
     }
 }
