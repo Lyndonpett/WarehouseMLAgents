05e432cfaec6c09a74ceecb25a1a5bc63e635d1e
Isaac Green <zuderg@gmail.com>
2022-12-14 18:32:07 -0600

    wheel RBs and colliders, lift up/down

--- Assets/Scripts/Forklift.cs
+++ Assets/Scripts/Forklift.cs
@@ -5,20 +5,40 @@ using UnityEngine;
 public class Forklift : MonoBehaviour
 {
     public GameObject fork;
+    public float drivespeed = 5f;
+    public float turnspeed = 3f;
     public float forkSpeed = 2f;
-
+    public float forkStartPos;
+    public Rigidbody LFwheel, RFwheel, LBwheel, RBwheel;
     private void Start()
     {
         fork = transform.GetChild(0).gameObject;
-        print(fork);
+        LBwheel = transform.GetChild(2).gameObject.GetComponent<Rigidbody>();
+        LFwheel = transform.GetChild(3).gameObject.GetComponent<Rigidbody>();
+        RBwheel = transform.GetChild(4).gameObject.GetComponent<Rigidbody>();
+        RFwheel = transform.GetChild(5).gameObject.GetComponent<Rigidbody>();
+        forkStartPos = fork.transform.position.y;
     }
 
     // move up and down
     private void Update()
     {
 
-        if (Input.GetKey(KeyCode.E)) fork.transform.Translate(Vector3.up * forkSpeed * Time.deltaTime);
-        else if (Input.GetKey(KeyCode.C)) fork.transform.Translate(Vector3.down * forkSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.E) && fork.transform.position.y < forkStartPos + 8)
+        {
+            fork.transform.Translate(Vector3.up * forkSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.C) && fork.transform.position.y > forkStartPos)
+        {
+            fork.transform.Translate(Vector3.down * forkSpeed * Time.deltaTime);
+        }
 
+        if (Input.GetKey(KeyCode.W))
+        {
+            LBwheel.AddTorque(Vector3.forward);
+            LFwheel.AddTorque(Vector3.forward);
+            RBwheel.AddTorque(Vector3.forward);
+            RFwheel.AddTorque(Vector3.forward);
+        }
     }
 }
